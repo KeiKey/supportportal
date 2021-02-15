a687885af9b04daa53ed05df8f9a58e45d337f5b
keiselami <kei@aledia.ca>
2021-02-15 17:48:00 +0100

    Jwt token generated/ added another TODO

--- src/main/java/com/supportportal/api/v1/utlity/JwtTokenProvider.java
+++ src/main/java/com/supportportal/api/v1/utlity/JwtTokenProvider.java
@@ -1,5 +1,16 @@
 package com.supportportal.api.v1.utlity;
 
+import com.auth0.jwt.JWT;
+import com.supportportal.api.v1.model.UserPrincipal;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.supportportal.api.v1.constant.SecurityConstant.*;
+
 public class JwtTokenProvider {
 
     /**TODO:
@@ -9,8 +20,27 @@ public class JwtTokenProvider {
      */
     private final String secret = "KeiTestSecret12345!@[]";
 
-    public String generateJwtToken () {
-        return "test";
+    /**TODO:
+     * Find other jwt tokens options
+     */
+    public String generateJwtToken (UserPrincipal userPrincipal) {
+        String[] claims = getClaimsFromUser(userPrincipal);
+        return JWT.create()
+                .withIssuer(GET_ARRAYS_KEI)
+                .withAudience(GET_ARRAYS_ADMINISTRATION)
+                .withIssuedAt(new Date())
+                .withSubject(userPrincipal.getUsername())
+                .withArrayClaim(AUTHORITIES, claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(secret.getBytes()));
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+        List<String> authorities = new ArrayList<>();
+        for (GrantedAuthority grantedAuthority : userPrincipal.getAuthorities()){
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return authorities.toArray(new String[0]);
     }
 
 }
